40a98e739a71f6182b350b074122eb6ef65005c3
projectGit2024 <boinaavinashkumar@gmail.com>
2025-01-28 19:38:17 +0700

    mywork using stash

--- myjavaReg.java
+++ myjavaReg.java
@@ -1,1 +1,11 @@
+<<<<<<< Updated upstream
 nager work completed
+=======
+this is my code in day 1
+this is my code in day 2
+this is my code in day 3
+this is my code in day 4
+this is my code in day 5
+
+
+>>>>>>> Stashed changes
